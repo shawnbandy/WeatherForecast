b4ee31c67eeba02d5380e89f204d61043e7afef0
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-08-06 17:05:42 -0400

    figured out the API stuff, now need to make the HTML look nicer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,11 @@
 //! IMPORTANT:: have to have user input change the API call. For openweathermap.org, you have to use the geocoding API
 //! The geocoding API will give you the lat and lon needed to call the weather data
 
-
-var weatherSearchBtn = $("#submitBtn");
-var citySearchInput =  $("#citySearch");
-var cityListEl = $("#cityList");
-var resetBtn = $("#resetBtn");
-var cityDisplay = $("#cityDisplay");
-var todayDate = $("#todayDate");
-
-//! My API key is 509e23105bc9e70fb5c519f8f743f99f
-
+//? My API key is 509e23105bc9e70fb5c519f8f743f99f
 //?http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 //*^this is geocoding to get the coordinates
-
 //?"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
 //*^this is the main one
-
-"http://api.openweathermap.org/geo/1.0/direct?q={city name}%2C+US&limit=1&appid=509e23105bc9e70fb5c519f8f743f99f"
-
-// weatherSearchBtn.on("click", searchFunction)
-
-todayDate.text(moment().format("MMMM do, YYYY"))
-
-// weatherSearchBtn.on("click", addCityToList);
-// weatherSearchBtn.on("click", getGeoCoding(cityEnteredHolder));
-
-weatherSearchBtn.on("click", masterClick);
-
-resetBtn.on("click", function(){
-    localStorage.clear();
-    location = location;
-});
-
-function masterClick(event){
-    event.preventDefault;
-    addCityToList();
-    var latAndLon = getGeoCoding(cityEnteredHolder);
-    getCityWeather(latAndLon[0], latAndLon[1]);
-};
 
 
 // // TODO: Add catch if input value in the search bar is null 
@@ -51,7 +18,33 @@ function masterClick(event){
 // TODO: the 5 day forecast cards need to have the following:
     //*date, icon of weather conditions, temperature, wind speed, humidity
 // TODO: make the list re-arrangeable 
-// TODO: 
+// TODO:
+
+
+var weatherSearchBtn = $("#submitBtn");
+var citySearchInput =  $("#citySearch");
+var cityListEl = $("#cityList");
+var resetBtn = $("#resetBtn");
+var cityDisplay = $("#cityDisplay");
+var todayDate = $("#todayDate");
+var currentWeather = $("#currentWeather");
+
+
+
+todayDate.text(moment().format("MMMM do, YYYY"));
+
+weatherSearchBtn.on("click", function(event){
+    event.preventDefault;
+    addCityToList();
+    getGeoCoding(cityEnteredHolder);
+});
+
+
+resetBtn.on("click", function(){
+    localStorage.clear();
+    location = location;
+});
+
 
 //* i serves as the index and gives each thing the data type
 //*this checks to see if there is any localstorage 
@@ -61,7 +54,6 @@ if (localStorage.getItem("list") != null){
     i = localStorage.getItem("dataNumber");
     console.log(i);
 }
-
 
 
 var cityEnteredHolder;
@@ -97,34 +89,59 @@ function addCityToList(){
 
 
 //*this function gets the latitude and longitude of the city entered 
-var latLonResult;
 function getGeoCoding(cityEntered){
 
-    var requestURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityEntered + "%2C+US&limit=1&appid=509e23105bc9e70fb5c519f8f743f99f"
-    console.log(requestURL);
-    
-    fetch(requestURL)
+    var geoCodingURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityEntered + "%2C+US&limit=1&appid=509e23105bc9e70fb5c519f8f743f99f" 
+
+    fetch(geoCodingURL)
         .then(function(response){
-            return response.json();
+            return response.json()
         })
         .then(function(data){
             for (var i = 0; i < data.length; i++){
-                console.log(data[i]);
                 var result = [data[i].lat, data[i].lon];
-                console.log(result);
-                return result;
             }
-        });
+            getCurrentCityWeather(result)
+            get5DayForecast(result)
+        })
+
+};
+
+
+
+function getCurrentCityWeather(latAndLonArray){
+    console.log(latAndLonArray[0] + " " + latAndLonArray[1]);
+
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latAndLonArray[0] + "&lon=" + latAndLonArray[1] + "&appid=509e23105bc9e70fb5c519f8f743f99f"
+
+    fetch(requestURL)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+
+            currentWeather.append($("<p>").text(data.weather[0].description)) //!this will need to be the icon
+            currentWeather.append($("<p>").text("Temperature: " + kelvinToFahrenheit(data.main.temp).toFixed(2) + " "));
+            currentWeather.append($("<p>").text("Humidity: " + data.main.humidity + " "));
+            currentWeather.append($("<p>").text("Wind speed: " + data.wind.speed + " "));
+
+            
+
+
+        })
 
 }
 
+function get5DayForecast(latAndLonArray){
 
+    var fiveDayForecastURL = "api.openweathermap.org/data/2.5/forecast/daily?lat=" + latAndLonArray[0] + "&lon=" + latAndLonArray[1] + "&cnt=5&appid=509e23105bc9e70fb5c519f8f743f99f"
+    
 
-function getCityWeather(lat, lon){
-    console.log(latLonResult);
+}
 
-    var requestURL = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&appid=509e23105bc9e70fb5c519f8f743f99f"
-
+function kelvinToFahrenheit(kelvin){
+    return (9/5) * (kelvin - 273) + 32
 }
 
 
